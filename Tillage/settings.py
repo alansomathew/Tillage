d36ea135fb669a1d37fe8d975c227c1ce3ab7d8f
Alanso Mathew <80514608+alansomathew@users.noreply.github.com>
2022-09-17 09:12:58 +0530

    Update settings.py

--- Tillage/settings.py
+++ Tillage/settings.py
@@ -135,5 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER="your@email.com'
+EMAIL_HOST_USER="your@email.com"
 EMAIL_HOST_PASSWORD='yourPassword'
